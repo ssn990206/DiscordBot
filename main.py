caefafd18317aae2750b960ab6b7fc438d052a19
ssn990206 <49487248+ssn990206@users.noreply.github.com>
2022-09-02 18:20:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import discord
 from discord.ext import commands
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
 
-bot = commands.Bot(command_prefix='>')
+bot=commands.Bot(command_prefix=']')
 
 @bot.event
 async def on_ready():
     print(">> bot is online <<")
 
-bot.run("MTAxNTA5MjczOTAyNjcxODcyMA.GCedFU.-tvjKlA_dG3H58WgGpbNUdmrY5MsSL3c96dXrQ")
+bot.run("MTAxNTA5MjczOTAyNjcxODcyMA.GhVVjk.NNOEUTH43vSGkhJq2KM4Jv3XyeFuaWONau27kM")
